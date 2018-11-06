31588974811cb0a3811ae0f53337a84dca59556b
Mohit Arora <mohit.mca17.du@gmail.com>
2018-11-06 20:27:04 +0530

    updated

--- ecommerce/static/pics.py
+++ ecommerce/static/pics.py
@@ -52,9 +52,12 @@ import pprint
       "Product Details": "Blue and grey checked casual shir",
       "Material & Care": ['Linen', 'Machine-wash']
   },
-  {
+      {
       "Image": ['20.1.jpg', '20.2.jpg', '20.3.jpg', '20.4.jpg'],
-      "Brand": "Elixirnet",
+      "Brand": "Popnetic",
+      "Type":"Shirt",
+      "Category":"Women",
+      "Seller":"Wiztech Corp",
       "Short Description": "Women Pink Straight Regular Fit Solid Casual Shirt",
       "Description": "Pink solid casual shirt, has a cutaway collar, button placket, na pockets, long sleeves, curved hem",
       "Mrp": 2759,
@@ -64,9 +67,12 @@ import pprint
       "Product Details": "Pink solid casual shirt, has a cutaway collar, button placket, na pockets, long sleeves, curved hem.Complete The Look - This cotton tee from Jaipur Kurti will add an extra layer of warmth to your outfit. Create a chic look for your next date by pairing this shirt with slim jeans and patent pumps",
       "Material & Care": ['Cotton', 'Hand-wash']
   },
-  {
-      "Image": ['21.1.jpg', '21.2.jpg', '21.3.jpg', '21.4.jpg' ],
+      {
+      "Image": ['21.1.jpg', '21.2.jpg', '21.3.jpg', '21.4.jpg'],
       "Brand": "La Zorie",
+      "Type":"Top",
+      "Category":"Women",
+      "Seller":"Blumentor",
       "Short Description": "Women Dusty Pink Solid Top",
       "Description": "Dusty Pink solid woven regular top, has a tie-up neck, three-quarter sleeves",
       "Mrp": 699,
@@ -76,21 +82,27 @@ import pprint
       "Product Details": "La Zorie Dusty Pink solid woven regular top, has a tie-up neck, three-quarter sleeves.Spruce up your wardrobe with this high-quality long-sleeve top from La Zoire. This pink top is a great addition to your cool-weather wardrobe and looks great with your favourite pair of jeans for a laid-back lunch date look.",
       "Material & Care": ['Linen', 'Machine-wash']
   },
-  {
-      "Image": [ '22.1.jpg', '22.2.jpg', '22.3.jpg', '22.4.jpg' ],
+      {
+      "Image": ['22.1.jpg', '22.2.jpg', '22.3.jpg', '22.4.jpg'],
       "Brand": "Harpa",
+      "Type":"Top",
+      "Category":"Women",
+      "Seller":"Blumentor",
       "Short Description": "Women Burgundy Solid Top",
       "Description": "Burgundy solid woven regular top, has a V-neck, three-quarter sleeves",
       "Mrp": 1199,
       "Discount": 55,
       "Price": 539,
-      "Size": { 'S': 0, 'M': 25, 'L': 10, 'XL': 6 },
+      "Size": {'S': 0, 'M': 25, 'L': 10, 'XL': 6},
       "Product Details": "Harpa Burgundy solid woven regular top, has a V-neck, three-quarter sleeves.Increase your style quotient with this top-notch three-quarter sleeve top by Harpa. Grab a casual bite to eat outside in this burgundy piece with worn jeans and comfortable shoes.",
       "Material & Care": ['100% Polyester', 'Hand-wash']
   },
-  {
-      "Image": [ '23.1.jpg', '23.2.jpg', '23.3.jpg', '23.4.jpg' ],
+      {
+      "Image": ['23.1.jpg', '23.2.jpg', '23.3.jpg', '23.4.jpg'],
       "Brand": "Libas",
+      "Type":"Shirt",
+      "Category":"Women",
+      "Seller":"Mayazen",
       "Short Description": "Women Mustard Yellow Solid Casual Shirt With Embroidered Detail",
       "Description": "Mustard yellow solid casual shirt with embroidered detail, has a spread collar, a full button placket, three-quarter roll-up sleeves, high-low hem",
       "Mrp": 1399,
@@ -100,9 +112,12 @@ import pprint
       "Product Details": "Libas Mustard yellow solid casual shirt with embroidered detail, has a spread collar, a full button placket, three-quarter roll-up sleeves, high-low hem. The three-quarter sleeves on this tee from Libas provides extra comfort and style. Pair yours with distressed denims and Chelsea boots and enjoy cocktails and dinner with your friends.",
       "Material & Care": ['Rayon', 'Hand-wash']
   },
-  {
-      "Image": [ '24.1.jpg', '24.2.jpg', '24.3.jpg', '24.4.jpg' ],
+      {
+      "Image": ['24.1.jpg', '24.2.jpg', '24.3.jpg', '24.4.jpg'],
       "Brand": "Zima Leto",
+      "Type":"Dress",
+      "Category":"Women",
+      "Seller":"Elixirnet",
       "Short Description": "Women Navy Blue Solid Midi Sheath Dress",
       "Description": "Navy blue solid knitted midi sheath dress, has a round neck, sleeveless, concealed zip and button closure, straight hem",
       "Mrp": 1099,
@@ -112,9 +127,12 @@ import pprint
       "Product Details": "Zima Leto Navy blue solid knitted midi sheath dress, has a round neck, sleeveless, concealed zip and button closure, straight hem.Turn heads this season in this effortlessly stylish dress by Zima Leto. When you're going to an art gallery opening or the theater, wear this solid navy blue piece with platform heels and a trendy clutch.",
       "Material & Care": ['Polyester', 'Machine-wash']
   },
-  {
-      "Image": [ '25.1.jpg', '25.2.jpg', '25.3.jpg', '25.4.jpg' ],
+      {
+      "Image": ['25.1.jpg', '25.2.jpg', '25.3.jpg', '25.4.jpg'],
       "Brand": "DressBerry",
+      "Type":"Dress",
+      "Category":"Women",
+      "Seller":"Wiztech Corp",
       "Short Description": "Women Blue Solid Pinafore Dress",
       "Description": "Blue solid woven pinafore dress, has a square neck, sleeveless,flared hem",
       "Mrp": 1499,
@@ -124,9 +142,12 @@ import pprint
       "Product Details": "DressBerry Blue solid woven pinafore dress, has a square neck, sleeveless,flared hem. Make a name for yourself this season when you wear this DressBerry dress. The solid blue piece can be matched with patent pumps and a pretty purse when you have a semi-formal event to attend.",
       "Material & Care": ['Cotton', 'Machine-wash']
   },
-   {
-      "Image": [ '26.1.jpg', '26.2.jpg', '26.3.jpg', '26.4.jpg' ],
+      {
+      "Image": ['26.1.jpg', '26.2.jpg', '26.3.jpg', '26.4.jpg'],
       "Brand": "Veni Vidi Vici",
+      "Type":"Dress",
+      "Category":"Women",
+      "Seller":"Blumentor,
       "Short Description": "Women Maroon Solid Bardot Fit and Flare Dress",
       "Description": "Maroon solid knitted fit and flare dress, has off-shoulder styling, short sleeves, flared hem",
       "Mrp": 1890,
@@ -136,9 +157,12 @@ import pprint
       "Product Details": "Veni Vidi Vici Maroon solid knitted fit and flare dress, has off-shoulder styling, short sleeves, flared hem. You're sure to love the style and comfort of this lavish Veni Vidi Vici dress. Spruce up this solid maroon piece for work by layering it with a light sweater and some ballerinas.",
       "Material & Care": ['95% Polyester', 'Machine-wash']
   },
-   {
-      "Image": [ '27.1.jpg', '27.2.jpg', '27.3.jpg', '27.4.jpg' ],
+      {
+      "Image": ['27.1.jpg', '27.2.jpg', '27.3.jpg', '27.4.jpg'],
       "Brand": "Harpa",
+      "Type":"Dress",
+      "Category":"Women",
+      "Seller":"Blumentor",
       "Short Description": "Women Yellow Floral Print A-Line Dress",
       "Description": "Yellow floral print woven A-line dress with cut-out detail, has a round neck, three-quarter sleeves, button closure, slightly flared hem",
       "Mrp": 1499,
@@ -148,9 +172,12 @@ import pprint
       "Product Details": "Harpa Yellow floral print woven A-line dress with cut-out detail, has a round neck, three-quarter sleeves, button closure, slightly flared hem. Show off your great sense of fashion when you opt for this Harpa dress. Wear this printed yellow piece with a pair of heels or flats for a perfect lunch look.",
       "Material & Care": ['100% Polyester', 'Hand-wash']
   },
-   {
-      "Image": [ '28.1.jpg', '28.2.jpg', '28.3.jpg', '28.4.jpg' ],
+      {
+      "Image": ['28.1.jpg', '28.2.jpg', '28.3.jpg', '28.4.jpg'],
       "Brand": "Miss Chase",
+      "Type":"Dress",
+      "Category":"Women",
+      "Seller":"FashionTech",
       "Short Description": "Women Navy Striped Detail T-shirt Dress",
       "Description": "Navy striped detail knit T-shirt dress, has a round neck, three-quarter sleeves, straight hem",
       "Mrp": 1499,
@@ -160,9 +187,12 @@ import pprint
       "Product Details": "Miss Chase Navy striped detail knit T-shirt dress, has a round neck, three-quarter sleeves, straight hem. Showcase your great sense of fashion when you opt for this Miss Chase dress. Wear this navy piece with ballerinas and your favourite sweater for a cute date look.",
       "Material & Care": ['Cotton', 'Machine-wash']
   },
-   {
-      "Image": [ '29.1.jpg', '29.2.jpg', '29.3.jpg', '29.4.jpg' ],
+      {
+      "Image": ['29.1.jpg', '29.2.jpg', '29.3.jpg', '29.4.jpg'],
       "Brand": "Athena",
+      "Type":"",
+      "Category":"",
+      "Seller":"",
       "Short Description": "Women Navy Lace Sheath Dress",
       "Description": "Navy lace woven sheath dress, has a keyhole neck, three-quarter sleeves, concealed zip closure, an attached lining, straight hem",
       "Mrp": 2295,
@@ -172,7 +202,7 @@ import pprint
       "Product Details": "This timeless and elegant Athena dress is suitable for any season. Complement this piece with a pair of caged heels, your favourite bag and some minimalistic silver jewellery for a classy date ensemble.",
       "Material & Care": ['Nylon and Polyester blend', 'Hand-wash']
   }
- 
+
   ])
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
