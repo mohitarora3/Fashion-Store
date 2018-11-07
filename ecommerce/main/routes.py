7bb12bdf56811a69731ce8b2dce64b52b9be1079
Mohit Arora <mohit.mca17.du@gmail.com>
2018-11-08 01:03:42 +0530

    updated

--- ecommerce/main/routes.py
+++ ecommerce/main/routes.py
@@ -9,14 +9,16 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @main.route('/home')
 def home():
-  items = mongo.db.items
-  return render_template('home.html', items=items)
+  items = mongo.db.items.find({'Type':'Bedsheet'})
+  brands=mongo.db.items.distinct('Brand',{'Type':'Bedsheet'})
+  return render_template('home.html', items=items,brands=brands)
 
 
 @main.route('/item/<string:item_id>')
 def item(item_id):
   item = mongo.db.items.find_one_or_404({"_id": ObjectId(item_id)})
   reviews_exist = mongo.db.review.find({'item_id': ObjectId(item_id)}, {'_id': 0, 'reviews': 1}).count()
+  similar_products = mongo.db.items.find({'Category': item['Category'], 'Type': item['Type']}).limit(10)
   if reviews_exist:
     reviews_dict_cursor = mongo.db.review.aggregate([{'$project':
                                                       {
@@ -27,7 +29,7 @@ def item(item_id):
                                                       }])
   else:
     reviews_dict_cursor = None
-  return render_template('item.html', item=item, reviews_cursor=reviews_dict_cursor, title=item['Description'])
+  return render_template('item.html', item=item, reviews_cursor=reviews_dict_cursor, title=item['Description'], similar_products=similar_products)
 
 
 @main.route('/search_results', methods=['POST'])
