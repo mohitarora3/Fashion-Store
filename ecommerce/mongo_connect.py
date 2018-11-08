f5519cdf61e67b3958c2af8cb5497c14476e503b
Mohit Arora <mohit.mca17.du@gmail.com>
2018-11-08 17:55:06 +0530

    update

--- ecommerce/mongo_connect.py
+++ ecommerce/mongo_connect.py
@@ -203,7 +203,7 @@ item = mongo.db.items.find_one({"_id": ObjectId('5be1a25deaeeee255c987c5c')})
 similar_products = mongo.db.items.find({'Category': item['Category'], 'Type': item['Type'], '_id': {'$not': item['_id']}}).limit(3)
 for item in similar_products:
   print(item)
-'''
+
 brands = mongo.db.items.distinct('Price', {'Type': 'Bedsheet'})
 brands.sort()
 print(brands)
@@ -215,3 +215,7 @@ While(i < len(brands)):
   price.append(brands[i])
   i = i + n
 print(price)
+'''
+
+c = mongo.db.items.distinct('Color', {'Type': 'Bedsheet'})
+print(c)
