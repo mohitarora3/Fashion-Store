6a6b5880521348c9211545b1431e984d10be4a0c
Vidul <vidulkumar@gmail.com>
2018-11-09 20:38:40 +0530

    Update mongo_connect.py
    updated

--- ecommerce/mongo_connect.py
+++ ecommerce/mongo_connect.py
@@ -9,7 +9,7 @@ from flask import request
 import json
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = "mongodb://localhost:27017/myDatabase"
+app.config['MONGO_URI'] = "mongodb://vidulkumar:New2mlab@ds157493.mlab.com:57493/mydatabase"
 mongo = PyMongo(app)
 
 
@@ -211,7 +211,7 @@ n = math.ceil(len(brands) / 3)
 print(n)
 i = 0
 price = []
-While(i < len(brands)):
+while(i < len(brands)):
   price.append(brands[i])
   i = i + n
 print(price)
